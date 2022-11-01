8f7957f59004f1f9c1b7f9aa93774874516ca59d
NataliaDanciuc <nataliadanciuc112@gmail.com>
2022-11-01 14:08:58 +0200

    lab 3

--- Pages/Books/Create.cshtml.cs
+++ Pages/Books/Create.cshtml.cs
@@ -36,7 +36,7 @@ namespace Danciuc_Natalia_Lab2.Pages.Books
         // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
         public async Task<IActionResult> OnPostAsync(string[] selectedCategories)
         {
-            var newBook = new Book();
+            var newBook =  Book;
             if (selectedCategories != null)
             {
                 newBook.BookCategories = new List<BookCategory>();
@@ -49,16 +49,16 @@ namespace Danciuc_Natalia_Lab2.Pages.Books
                     newBook.BookCategories.Add(catToAdd);
                 }
             }
-            if (await TryUpdateModelAsync<Book>(
+           /* if (await TryUpdateModelAsync<Book>(
             newBook,
             "Book",
             i => i.Title, i => i.Author,
             i => i.Price, i => i.PublishingDate, i => i.PublisherID))
-            {
+            {*/
                 _context.Book.Add(newBook);
                 await _context.SaveChangesAsync();
                 return RedirectToPage("./Index");
-            }
+           // }
             PopulateAssignedCategoryData(_context, newBook);
             return Page();
         }
