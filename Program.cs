01fe11bf8639a62f1297ef2816941391a961b13e
NataliaDanciuc <nataliadanciuc112@gmail.com>
2022-11-18 16:23:56 +0200

    +primele 2 sarcini

--- Program.cs
+++ Program.cs
@@ -17,6 +17,8 @@ builder.Services.AddRazorPages(options =>
    options.Conventions.AllowAnonymousToPage("/Books/Index");
    options.Conventions.AllowAnonymousToPage("/Books/Details");
     options.Conventions.AuthorizeFolder("/Members", "AdminPolicy");
+    options.Conventions.AuthorizeFolder("/Publishers", "AdminPolicy");
+    options.Conventions.AuthorizeFolder("/Categories", "AdminPolicy");
 });
 builder.Services.AddDbContext<Danciuc_Natalia_Lab2Context>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("Danciuc_Natalia_Lab2Context") ?? throw new InvalidOperationException("Connection string 'Danciuc_Natalia_Lab2Context' not found.")));
